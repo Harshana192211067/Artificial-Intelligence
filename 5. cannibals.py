f4ab629cab7bada1f6787bb06816760f9c3744b7
Harshana192211067 <123436487+Harshana192211067@users.noreply.github.com>
2024-03-31 14:24:45 +0530

    Add files via upload

--- 5. cannibals.py
+++ 5. cannibals.py
@@ -1,61 +1,91 @@
-from collections import deque
+issionaries\n3. The boat cannot cross the river by itself with no people on board") 
+lM = 3		 #lM = Left side Missionaries number 
+lC = 3		 #lC = Laft side Cannibals number 
+rM=0		 #rM = Right side Missionaries number 
+rC=0		 #rC = Right side cannibals number 
+userM = 0	 #userM = User input for number of missionaries for right to left side travel 
+userC = 0	 #userC = User input for number of cannibals for right to left travel 
+k = 0
+print("\nM M M C C C |	 --- | \n") 
+try: 
+	while(True): 
+		while(True): 
+			print("Left side -> right side river travel") 
+			#uM = user input for number of missionaries for left to right travel 
+			#uC = user input for number of cannibals for left to right travel 
+			uM = int(input("Enter number of Missionaries travel => "))	 
+			uC = int(input("Enter number of Cannibals travel => ")) 
 
-class State:
-    def __init__(self, missionaries, cannibals, boat):
-        self.missionaries = missionaries
-        self.cannibals = cannibals
-        self.boat = boat
+			if((uM==0)and(uC==0)): 
+				print("Empty travel not possible") 
+				print("Re-enter : ") 
+			elif(((uM+uC) <= 2)and((lM-uM)>=0)and((lC-uC)>=0)): 
+				break
+			else: 
+				print("Wrong input re-enter : ") 
+		lM = (lM-uM) 
+		lC = (lC-uC) 
+		rM += uM 
+		rC += uC 
 
-    def __eq__(self, other):
-        return (self.missionaries, self.cannibals, self.boat) == (other.missionaries, other.cannibals, other.boat)
+		print("\n") 
+		for i in range(0,lM): 
+			print("M ",end="") 
+		for i in range(0,lC): 
+			print("C ",end="") 
+		print("| --> | ",end="") 
+		for i in range(0,rM): 
+			print("M ",end="") 
+		for i in range(0,rC): 
+			print("C ",end="") 
+		print("\n") 
 
-    def __hash__(self):
-        return hash((self.missionaries, self.cannibals, self.boat))
+		k +=1
 
-def is_valid_state(state):
-    return 0 <= state.missionaries <= 3 and 0 <= state.cannibals <= 3 and (state.missionaries >= state.cannibals or state.missionaries == 0) and (3 - state.missionaries >= 3 - state.cannibals or state.missionaries == 3)
+		if(((lC==3)and (lM == 1))or((lC==3)and(lM==2))or((lC==2)and(lM==1))or((rC==3)and (rM == 1))or((rC==3)and(rM==2))or((rC==2)and(rM==1))): 
+			print("Cannibals eat missionaries:\nYou lost the game") 
 
-def generate_next_states(current_state):
-    moves = [(1, 0), (0, 1), (1, 1), (2, 0), (0, 2)]
-    next_states = []
-    for move in moves:
-        if current_state.boat == 'left':
-            next_state = State(current_state.missionaries - move[0], current_state.cannibals - move[1], 'right')
-        else:
-            next_state = State(current_state.missionaries + move[0], current_state.cannibals + move[1], 'left')
-        if is_valid_state(next_state):
-            next_states.append(next_state)
-    return next_states
+			break
 
-def solve_missionaries_cannibals():
-    initial_state = State(3, 3, 'left')
-    goal_state = State(0, 0, 'right')
+		if((rM+rC) == 6): 
+			print("You won the game : \n\tCongrats") 
+			print("Total attempt") 
+			print(k) 
+			break
+		while(True): 
+			print("Right side -> Left side river travel") 
+			userM = int(input("Enter number of Missionaries travel => ")) 
+			userC = int(input("Enter number of Cannibals travel => ")) 
+			
+			if((userM==0)and(userC==0)): 
+					print("Empty travel not possible") 
+					print("Re-enter : ") 
+			elif(((userM+userC) <= 2)and((rM-userM)>=0)and((rC-userC)>=0)): 
+				break
+			else: 
+				print("Wrong input re-enter : ") 
+		lM += userM 
+		lC += userC 
+		rM -= userM 
+		rC -= userC 
 
-    queue = deque([(initial_state, [])])
-    visited = set()
+		k +=1
+		print("\n") 
+		for i in range(0,lM): 
+			print("M ",end="") 
+		for i in range(0,lC): 
+			print("C ",end="") 
+		print("| <-- | ",end="") 
+		for i in range(0,rM): 
+			print("M ",end="") 
+		for i in range(0,rC): 
+			print("C ",end="") 
+		print("\n") 
 
-    while queue:
-        current_state, path = queue.popleft()
-        if current_state == goal_state:
-            return path + [current_state]
+	
 
-        if current_state not in visited:
-            visited.add(current_state)
-            next_states = generate_next_states(current_state)
-            for next_state in next_states:
-                queue.append((next_state, path + [current_state]))
-
-    return None
-
-def print_solution(path):
-    if path is None:
-        print("No solution found.")
-        return
-
-    print("Solution path:")
-    for state in path:
-        print(state.missionaries, state.cannibals, state.boat)
-
-# Solve the problem
-solution_path = solve_missionaries_cannibals()
-print_solution(solution_path)
+		if(((lC==3)and (lM == 1))or((lC==3)and(lM==2))or((lC==2)and(lM==1))or((rC==3)and (rM == 1))or((rC==3)and(rM==2))or((rC==2)and(rM==1))): 
+			print("Cannibals eat missionaries:\nYou lost the game") 
+			break
+except EOFError as e: 
+	print("\nInvalid input please retry !!")
